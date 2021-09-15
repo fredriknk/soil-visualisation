b1b93ec8dd06f94e768fa91d0af406386c1ee897
Fredrik Kleven <fredrik.kleven@outlook.com>
2021-09-15 11:26:26 +0200

    Added interpolation, averaging and stdev

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def matrixplot(df,
                plotcolumns = ["N2O_N_mug_m2h", "CO2_C_mug_m2h"],
                group1 ="treatment",
                subgroup ='nr',
-               avg_plots = "false",
+               params = "ASI",
                figsize=(20, 15)):
 
     nPlots = len(df[group1].unique())
@@ -27,20 +27,45 @@ def matrixplot(df,
     for i_gas, gas in enumerate(plotcolumns):
         axs[i_gas, 0].set_ylabel(gas)
         for treatment in np.sort(df[group1].unique()):
-            if avg_plots:
-                df[(df[group1] == treatment)].groupby([subgroup])[gas].plot(title=treatment, ax=axs[i_gas, treatment - 1], )
+            dx = df[df[group1] == treatment]
+
+            if "I" in params:
+                dx = dx.interpolate()
+
+            if "A" not in params:
+                dx.groupby([subgroup])[gas].plot(title=treatment, ax=axs[i_gas, treatment - 1], )
+            else:
+                dx = dx[gas]
+                mean_v = dx.groupby(dx.index.date).mean()
+                std_dev = dx.groupby(dx.index.date).std()
+                min_v =  dx.groupby(dx.index.date).min()
+                max_v = dx.groupby(dx.index.date).max()
+                mean_v.plot(title=treatment, ax=axs[i_gas, treatment - 1])
+                if "S" in params:
+                    axs[i_gas, treatment - 1].fill_between(std_dev.index,mean_v+std_dev,mean_v-std_dev,color='blue',alpha = 0.2)
+                else:
+                    axs[i_gas, treatment - 1].fill_between(min_v.index, min_v, max_v, color='blue', alpha=0.2)
 
     plt.tight_layout()
     plt.subplots_adjust(wspace=0.02, hspace=0)
     plt.show()
 
 if __name__ == "__main__":
-    df = toDf("Copy of capture_slopes.xls",
-              date_column = "date")
+    filename = "Copy of capture_slopes.xls" #filename for raw data
+    date_column = "date"
+    plotcolumns = ["N2O_N_mug_m2h", "CO2_C_mug_m2h"] #the collumns in the excel document to be parsed
+    group1 = "treatment" # The first group to sort data by
+    subgroup = 'nr' # the second group to sort data by
+    params = "AI" # A = average, S = stdev, I = Interpolate
+    figsize = (20, 15)
+
+    df = toDf(filename,
+              date_column)
 
     matrixplot(df,
-               plotcolumns = ["N2O_N_mug_m2h", "CO2_C_mug_m2h"],
-               group1 ="treatment",
-               subgroup ='nr',
-               avg_plots = "false",
-               figsize=(20, 15))
+               plotcolumns ,
+               group1,
+               subgroup,
+               params,
+               figsize)
+
