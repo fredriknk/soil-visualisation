7c296fc26c3e4bf5af65e42c113e769e6f682a8e
Fredrik Kleven <fredrik.kleven@outlook.com>
2021-09-16 10:10:21 +0200

    fixed title bug

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ matplotlib.use('TkAgg') # plot as external window
 import matplotlib.pyplot as plt
 plt.ioff()
 import numpy as np
+from time import time
 from multiprocessing import Pool
 from scipy import interpolate
 from math import sqrt,floor,ceil
@@ -15,7 +16,7 @@ def toDf(filename="capture_slopes.xls",date_column = "date"):
     df=df.set_index([date_column])
     return df
 
-def plotDF(dt,group1,gas,treatment,params,ax):
+def plotDF(dt,subgroup,gas,params,ax, title):
 
     if "I" in params:  # Interpolate missing values
         dt = dt.interpolate()
@@ -28,12 +29,8 @@ def plotDF(dt,group1,gas,treatment,params,ax):
         min_v = dx.groupby(dx.index.date).min()
         max_v = dx.groupby(dx.index.date).max()
 
-        if i_gas == 0:
-            title = treatment
-        else:
-            title = ""
+        mean_v.plot(title = title,ax=ax)
 
-        mean_v.plot(title=title, ax=axs[i_gas, treatment - 1])
         color = "blue"
         if "S" in params:
             ax.fill_between(std_dev.index, mean_v + std_dev, mean_v - std_dev, color=color,
@@ -43,9 +40,12 @@ def plotDF(dt,group1,gas,treatment,params,ax):
             ax.fill_between(min_v.index, min_v, max_v, color=color, alpha=0.2)
             color = "green"
     if "R" in params:  # If not average, just print all plots individually
-        dt.groupby([subgroup])[gas].plot(title=treatment, ax=ax )
+        dt.groupby([subgroup])[gas].plot(title = title,ax=ax )
 
     return ax
+
+def plotDF_wrapper(args):
+   return plotDF(*args)
 
 def matrixplot(df,
                plotcolumns = ["N2O_N_mug_m2h", "CO2_C_mug_m2h"],
@@ -58,14 +58,21 @@ def matrixplot(df,
 
     fig, axs = plt.subplots(nrows=len(plotcolumns), ncols=nPlots, figsize=figsize, sharex=True,
                             sharey='row')  # ,sharey=True)
-    pool = Pool(4)
+
     for i_gas, gas in enumerate(plotcolumns):
         axs[i_gas, 0].set_ylabel(gas)
 
-        for treatment in np.sort(df[group1].unique()):
-            dt = df[df[group1] == treatment]
-            axs[i_gas, treatment - 1] = plotDF(dt,group1,gas,treatment,params,axs[i_gas, treatment - 1])
 
+
+        for treatment in np.sort(df[group1].unique()):
+
+            if i_gas == 0:
+                title = treatment
+            else:
+                title = ""
+
+            dt = df[df[group1] == treatment]
+            axs[i_gas, treatment - 1] = plotDF(dt,subgroup,gas,params,axs[i_gas, treatment - 1],title)
 
     fig.autofmt_xdate(rotation=70)
     plt.tight_layout()
@@ -73,12 +80,13 @@ def matrixplot(df,
     plt.show()
 
 if __name__ == "__main__":
+    start = time()
     filename = "data/Copy of capture_slopes.xls" #filename for raw data
     date_column = "date"
     plotcolumns = ["N2O_N_mug_m2h", "CO2_C_mug_m2h"] #the collumns in the excel document to be parsed
     group1 = "treatment" # The first group to sort data by
     subgroup = 'nr' # the second group to sort data by
-    params = "RI" # A = Average, S = Stdev, I = Interpolate, R = Regular (all graphs superimposed) X = Minmax
+    params = "ASIRX" # A = Average, S = Stdev, I = Interpolate, R = Regular (all graphs superimposed) X = Minmax
     figsize = (20, 15)
 
     df = toDf(filename,
@@ -91,3 +99,4 @@ if __name__ == "__main__":
                params,
                figsize)
 
+    print(time()-start)
